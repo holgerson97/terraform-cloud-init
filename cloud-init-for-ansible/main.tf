ebc726886bfcba8e7d973da013b81b48b3640f20
Nils Carstensen <nilscarstensen@live.com>
2021-03-14 10:30:34 +0100

    init

---
+++ cloud-init-for-ansible/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.1.0"
+    }
+  }
+}
+
+provider "random" {
+
+}
+
+resource "random_password" "server-password" {
+
+  length           = 25
+  special          = true
+
+}
+
+data "template_file" "user-data" {
+
+    template = file("${path.module}/templates/cloud_init.yml")
+
+    vars = {
+
+        root_ssh_key    = var.root-ssh-key
+        ansible_ssh_key = var.ansible-ssh-key
+        server_passwd   = random_password.server-password
+
+    }
+}
---
+++ cloud-init-for-ansible/variables.tf
@@ -0,0 +1,14 @@
+variable "root-ssh-key" {
+
+    type      = string
+    sensitive = true
+  
+}
+
+variable "ansible-ssh-key" {
+
+    type      = string
+    sensitive = true
+  
+}
+
